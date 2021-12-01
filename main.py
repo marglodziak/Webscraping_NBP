9b19492b48c95b61fdba26cd52e4ad39f4f014aa
Marcin <marcinglodziak@gmail.com>
2021-12-01 20:31:39 +0100

    Version with simple tkinter GUI and calendar

--- main.py
+++ main.py
@@ -1,46 +1,251 @@
 from selenium import webdriver
 import time
+import datetime
+import tkinter as tk
+import tkinter.font as font
 from bs4 import BeautifulSoup
+
 PATH = "./chromedriver"
 
+root = tk.Tk()
+root.configure(bg="#a7ab38")
+root.title("Webscraping NBP")
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+root.geometry("%dx%d" % (width, height))
 
-def current_day():
+# czcionki
+entryFont = font.Font(size=30, family="Rasa")
+welcomeFont = font.Font(size=25, family="Rasa")
+welcomeFontBold = font.Font(size=25, family="Open Sans", weight="bold")
+mainFont = font.Font(size=20, family="Rasa")
+
+# ramki
+frameMain = tk.Frame(root, bg="#a7ab38")
+frameAddWord = tk.Frame(root, bg="#a7ab38")
+frameRandomWord = tk.Frame(root, bg="#a7ab38")
+frameTestYourself = tk.Frame(root, bg="#a7ab38")
+infoFrame = tk.Frame(root, bg="#a7ab38")
+currencyFrame = tk.Frame(root, bg="#a7ab38")
+amountFrame = tk.Frame(root, bg="#a7ab38")
+calendarFrame = tk.Frame(amountFrame)
+resultsFrame = tk.Frame(root, bg="#a7ab38")
+
+calendarFrame.grid()
+currencyList = ['dolar amerykański', 'euro', 'frank szwajcarski', 'funt szterling']
+chosenCurrency = ''
+chosenDate = ''
+framesDict = {}
+
+
+# ----- FUNKCJE POMOCNICZE -----
+
+
+# funkcja chowająca i pokazująca ramki z oknami
+def showAndHideFrames(frameToShow, frameToHide=frameMain):
+    if frameToHide is None:
+        frameToShow.grid()
+        return
+    if frameToHide != frameMain and frameToHide not in framesDict.values():
+        for child in frameToHide.winfo_children():
+            child.destroy()
+    frameToHide.grid_remove()
+    frameToShow.grid()
+
+
+# Funkcja wypisująca wyniki
+def wypisz(rate, date, rate_date, earnings):
+    dateComplete = "Data zamówienia: " + date
+    earningsComplete = "Zarobek: " + str(earnings)
+    rateComplete = "Kurs z dnia poprzedzającego ("+chosenCurrency+', '+rate_date+"): " + str(rate)
+    gainComplete = "Przychód należny: " + str(round(earnings * rate, 2))
+
+    showAndHideFrames(resultsFrame, amountFrame)
+    introLabel = tk.Label(resultsFrame, text="Dane do ewidencji:\n")
+    dateLabel = tk.Label(resultsFrame, text=dateComplete)
+    earningsLabel = tk.Label(resultsFrame, text=earningsComplete)
+    rateLabel = tk.Label(resultsFrame, text=rateComplete)
+    gainLabel = tk.Label(resultsFrame, text=gainComplete)
+    returnButton = tk.Button(resultsFrame, text="Powrót do ekranu głównego", command=lambda:[showAndHideFrames(frameMain, resultsFrame)])
+
+    introLabel.grid(row=0)
+    dateLabel.grid(row=1)
+    earningsLabel.grid(row=2)
+    rateLabel.grid(row=3)
+    gainLabel.grid(row=4)
+    returnButton.grid(row=5)
+
+
+# Sekcja "O programie"
+def showInfo():
+    showAndHideFrames(infoFrame)
+    infoLabel = tk.Label(infoFrame, anchor='w', justify="left",
+                         text='Witaj w programie Kursy Walut! \nMój program ułatwi Ci wyszukanie odpowiedniego kursu, '
+                         'jeśli nie zarabiasz w złotówkach i prowadzisz ewidencję z uwzględnieniem aktualnych kursów '
+                         'walut, podanych na stronie NBP. \n\n'
+                         'Instrukcja obsługi:\n'
+                         '1) Wybierz odpowiednią walutę spośród podanych.\n'
+                         '2) Wybierz odpowiednią datę.\n'
+                         '3) Podaj zarobioną kwotę.\n\n'
+                         'Po wykonaniu szeregu operacji, program poda na wyjściu kurs waluty z odpowiedniego dnia oraz '
+                         'przeliczoną na złotówki kwotę, którą możesz wpisać do ewidencji. Powodzenia!')
+    returnButton = tk.Button(infoFrame, text="Powrót do ekranu głównego",
+                             command=lambda: [showAndHideFrames(frameMain, infoFrame)])
+    infoLabel.grid(row=1, column=0)
+    returnButton.grid(row=2)
+
+
+# Funkcja zwracająca liczbę dni w danym miesiącu
+def howManyDays(month, year):
+    if month in (1, 3, 5, 7, 8, 10, 12):
+        return 31
+    if month in (4, 6, 9, 11):
+        return 30
+    if przestepny(year):
+        return 29
+    return 28
+
+
+# Funkcja tworząca nową ramkę kalendarza
+def createFrame(year, month):
+    if framesDict.get(str(year)+'-'+str(month)) is None:
+        month = int(month)
+        year = int(year)
+        newFrame = tk.Frame(calendarFrame)
+        buttons = []
+
+        def saveDate(day, month, year):
+            buttons[day+j-1]['state'] = tk.DISABLED
+            if month == datetime.datetime.today().month:
+                for button in buttons[:datetime.datetime.today().day+j-1]:
+                    if button != buttons[day+j-1]:
+                        button['state'] = tk.NORMAL
+            else:
+                for button in buttons:
+                    if button != buttons[day+j-1]:
+                        button['state'] = tk.NORMAL
+
+            global chosenDate
+            chosenDate = str(day) + '-' + str(month) + '-' + str(year)
+        j = 0
+        for i in range(datetime.datetime(int(year), int(month), 1).weekday()):
+            buttons.append(tk.Button(newFrame, text=""))
+            j += 1
+
+        for i in range(j, howManyDays(month, year)+j):
+            buttons.append(tk.Button(newFrame, text=(i - j + 1), command=lambda i=i: [saveDate(i-j + 1, month, year)]))
+        if datetime.datetime.today().month == month:
+            for button in buttons[j+datetime.datetime.today().day:]:
+                button['command'] = ''
+                button['text'] = ''
+        tk.Label(newFrame, text=month).grid(row=0, column=0)
+        tk.Button(newFrame, text='Poprzedni', command=lambda: [changeMonth(year, month-1, month,buttons)]).grid(row=0, column=1, columnspan=3)
+        if month != datetime.datetime.today().month:
+            tk.Button(newFrame, text='Następny', command=lambda: [changeMonth(year, month+1, month,buttons)]).grid(row=0, column=4, columnspan=3)
+        for i in range(len(buttons)):
+            buttons[i].grid(row=(i // 7)+1, column=i % 7)
+        framesDict[str(year)+'-'+str(month)] = newFrame
+    return framesDict[str(year)+'-'+str(month)]
+
+
+# Funkcja zmieniająca okno kalendarza
+def changeMonth(year, month, previousMonth, buttons=[]):
+    if previousMonth != month:
+        for button in buttons:
+            button['state'] = tk.NORMAL
+    previousYear = year
+    if month == 13:
+        month = 1
+        year += 1
+    if month == 0:
+        month = 12
+        year -= 1
+    currentFrame = framesDict.get(str(previousYear)+'-'+str(previousMonth))
+    newFrame = createFrame(year, month)
+    showAndHideFrames(newFrame, currentFrame)
+
+
+# Funkcja zapisująca wybraną walutę do zmiennej globalnej
+def saveCurrency(nr):
+    global chosenCurrency
+    chosenCurrency = currencyList[nr]
+    chooseAmount()
+
+
+# Obsługa okna wybrania waluty
+def chooseCurrency():
+    showAndHideFrames(currencyFrame)
+    global chosenCurrency
+    i = 0
+    for el in currencyList:
+        tk.Button(currencyFrame, text=el, command=lambda i=i: [saveCurrency(i)]).grid(row=i)
+        i += 1
+
+
+# Obsługa okna wybierania daty i kwoty
+def chooseAmount():
+    def saveAmount():
+        previous_date = chosenDate
+        amount = float(entryAmount.get())
+        rate = 0
+        triesNumber = 0
+        while not rate and triesNumber != 5:
+            previous_date = previous_day(previous_date)
+            tables = choose_month(previous_date)
+            href = get_href(tables, previous_date)
+            rate = get_rate(href, previous_date)
+            triesNumber += 1
+        if triesNumber == 5:
+            showAndHideFrames(resultsFrame, amountFrame)
+            introLabel = tk.Label(resultsFrame, text="Nie znaleziono odpowiednich danych. Przepraszam.")
+            returnButton = tk.Button(resultsFrame, text="Powrót do ekranu głównego",
+                                     command=lambda: [showAndHideFrames(frameMain, resultsFrame)])
+
+            introLabel.grid(row=0)
+            returnButton.grid(row=5)
+        else:
+            wypisz(rate, chosenDate, previous_date, amount)
+
+    showAndHideFrames(amountFrame, currencyFrame)
     daytime = time.strftime("%Y-%m-%d")
     daytime = daytime.split('-')
     TY = daytime[0]
     TM = daytime[1]
-    TD = daytime[2]
+    changeMonth(int(TY), int(TM), int(TM))
+    amountLabel = tk.Label(amountFrame, text="Podaj zarobioną kwotę: ")
+    entryAmount = tk.Entry(amountFrame)
+    amountLabel.grid(row=1, column=0)
+    entryAmount.grid(row=1, column=1)
+    saveButton = tk.Button(amountFrame, text="Wybierz", command=saveAmount)
+    saveButton.grid(row=2, column=2)
 
 
-def get_day():
-    data = input("Podaj datę w formacie DD-MM-YYYY: ")
-    data_s = data.split('-')
-    previous_day(data)
-
-
+# Czy rok przestępny
 def przestepny(year):
     return (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
 
 
+# Cofnięcie się o jeden dzień
 def previous_day(current_date):
-    # Cofnięcie o jeden dzień
     current_date = current_date.split('-')
     day = current_date[0]
     month = current_date[1]
     year = current_date[2]
     day = int(day)
+    month = int(month)
     day -= 1
     if day == 0:
-        if int(month) in [1, 2, 4, 6, 9, 11]:
+        if month in (1, 2, 4, 6, 9, 11):
             day = 31
-        if int(month) == 3:
-            if przestepny(year):
-                day = 29
-            else:
-                day = 28
-        if int(month) in [5, 7, 8, 10, 12]:
+        elif month in (5, 7, 8, 10, 12):
             day = 30
-        month = int(month)
+        elif przestepny(year):
+            day = 29
+        else:
+            day = 28
+
         month -= 1
         if month == 0:
             month = 12
@@ -55,6 +260,7 @@ def previous_day(current_date):
     return day+'-'+month+'-'+year
 
 
+# Funkcja rozbijająca datę na części
 def get_date(date):
     date = date.split('-')
     day = date[0]
@@ -63,6 +269,7 @@ def get_date(date):
     return day, month, year
 
 
+# Funkcja zwracająca tablicę z całego miesiąca
 def choose_month(date):
     day, month, year = get_date(date)
     driver = webdriver.Chrome(PATH)
@@ -78,6 +285,7 @@ def choose_month(date):
     return tables
 
 
+# Funkcja zwracająca odnośnik do odpowiedniej tabeli
 def get_href(tables, date):
     day, month, year = get_date(date)
     href = ''
@@ -89,11 +297,11 @@ def get_href(tables, date):
                 href = href.replace("%amp;", "&")
                 break
     if href == '':
-        # print("Błąd! Nie znaleziono odpowiednich danych.")
         return 0
     return href
 
 
+# Funkcja pobierająca odpowiedni kurs waluty z tabeli
 def get_rate(href, date):
     day, month, year = get_date(date)
     driver = webdriver.Chrome(PATH)
@@ -113,7 +321,7 @@ def get_rate(href, date):
         if len(split) > 1:
             currency = split[1].split('<')
             currency = currency[0]
-        if currency == 'dolar amerykański':  # zmienna globalna waluta #
+        if currency == chosenCurrency:  # zmienna globalna waluta #
             row_td2 = row.find_all('td', class_='right')
             row_td2 = str(row_td2[1]).split(">")
             row_td2 = row_td2[1].split('<')
@@ -123,26 +331,32 @@ def get_rate(href, date):
             return rate
 
 
-def wypisz(rate, date, rate_date):
-    day, month, year = get_date(date)
-    earnings = float(input("Wprowadź zarobek w dolarach: "))
-    print("Dane do ewidencji:\n")
-    print("Data zamówienia: ", day + '.' + month + '.' + year)
-    print("Zarobek: ", earnings)
-    print("Kurs dolara z dnia poprzedzającego ("+rate_date+"): ", rate)
-    print("Przychód należny: ", round(earnings * rate, 2))
-
-
 if __name__ == "__main__":
-    input_date = input("Podaj datę w formacie DD-MM-YYYY: ")
-    previous_date = input_date
-    rate = 0
-    while not rate:
-        previous_date = previous_day(previous_date)
-        tables = choose_month(previous_date)
-        href = get_href(tables, previous_date)
-        rate = get_rate(href, previous_date)
-    wypisz(rate, input_date, previous_date)
 
+    # ------------- EKRAN GŁÓWNY -------------
 
+    # ekran główny, elementy
+    frameMain.grid(row=0, column=0, columnspan=3, sticky='')
+    welcomeText = tk.Label(frameMain, bg="#a7ab38",fg="#fdffba",
+                           text="Witaj w słowniku.\n\nWybierz z poniższych opcji, co chcesz zrobić:",
+                           font=welcomeFontBold)
 
+    info = tk.Button(frameMain, text="O programie", font=welcomeFont, bg="#fffdbd", fg="#5c2500",
+                     activebackground="#e3e08f", activeforeground="#5c2500", pady=10, padx=20, command=showInfo)
+    chooseCurrency = tk.Button(frameMain, text="Wybierz walutę", font=welcomeFont, bg="#fffdbd", fg="#475b11",
+                               activebackground="#e3e08f", activeforeground="#475b11", pady=10, padx=20,
+                               command=chooseCurrency)
+    checkRates = tk.Button(frameMain, text="Zbadaj kurs waluty", font=welcomeFont, bg="#fffdbd", fg="#030054",
+                           activebackground="#e3e08f", activeforeground="#030054", pady=10, padx=20)#, command=show)
+    close = tk.Button(frameMain, text="Wyjdź", font=welcomeFont, bg="#fffdbd", pady=10, fg="#a30000",
+                      activebackground="#910000", activeforeground="white", padx=20, command=root.destroy)
+
+    # ekran główny, ułożenie elementów
+    welcomeText.grid(row=0, column=0, columnspan=4, sticky="")
+    info.grid(row=1, column=0, padx=(0, 40), pady=80)
+    chooseCurrency.grid(row=1, column=1, padx=40)
+
+    checkRates.grid(row=1, column=2, padx=(0, 40))
+    close.grid(row=2, column=2)
+
+    root.mainloop()
